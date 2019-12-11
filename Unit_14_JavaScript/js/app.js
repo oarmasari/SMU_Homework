f51ef9c8c74b6e2dbdb9c3d2f6058c0bbf8d2dff
Olesea Armasari <oarmasari@gmail.com>
2019-12-11 10:04:46 -0600

    Unit_14_Javascript

---
+++ Unit_14_JavaScript/js/app.js
@@ -0,0 +1,39 @@
+// variable for data file 
+var UFO_table = data;
+
+
+// variable to select tbody in html
+var tbody = d3.select("tbody");
+
+
+// function one 
+function pull_data(aliens) {
+    tbody.html("");
+    console.log(aliens);
+    aliens.forEach(function(appending) {
+        tbody.append("tr");
+        Object.entries(appending).forEach(function([key, value]) {
+            tbody.append("td").text(value);
+        })
+    })
+}
+
+
+//function two 
+function filt_data() {
+    d3.event.preventDefault();
+    var filtered_data = UFO_table;
+    // console.log("UFO Sightings");
+    var date_input = d3.select("#datetime").property("value");
+    console.log(date_input);
+    if (date_input) {
+        filtered_data = filtered_data.filter(temporary_row => temporary_row.datetime === date_input);
+    }
+    pull_data(filtered_data);
+    console.log(filtered_data);
+}
+
+pull_data(UFO_table)
+
+d3.selectAll("#filter-btn").on("click", 
+
